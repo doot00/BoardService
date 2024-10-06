9c2fab90f2d1a29f44d1cc96cad6c95b317cb3d2
doot00 <binnung_s@naver.com>
2024-10-06 17:32:09 +0900

    마크다운
    마크다운 설치, 컴포넌트 작성 및 question_detail.html에 적용

---
+++ src/main/groovy/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+    public String markdown(String markdown) { // 마크다운 메서드 생성, HTML문서로 변환하여 리턴, 일반 텍스트를 변환된 HTML로 리턴
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
