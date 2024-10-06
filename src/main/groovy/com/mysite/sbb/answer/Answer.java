972b92a11f6f0a45893b42f898d1b01abcb82bb6
doot00 <binnung_s@naver.com>
2024-10-06 15:50:10 +0900

    질문 답변
    수정 / 삭제 기능 추가

--- src/main/groovy/com/mysite/sbb/answer/Answer.java
+++ src/main/groovy/com/mysite/sbb/answer/Answer.java
@@ -2,16 +2,12 @@ package com.mysite.sbb.answer;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import com.mysite.sbb.question.Question;
 
 import com.mysite.sbb.user.SiteUser;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.cglib.core.Local;
@@ -36,4 +32,7 @@ public class Answer {
     private SiteUser author;
 
     private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
